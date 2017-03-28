dda9265a90a997fef6abc8b53935fc6894e79c9a
Santiago Estens <hdrev@users.noreply.github.com>
2017-03-28 01:48:17 -0500

    Innitial commit of the sentiment analysis class.

---
+++ SentimentAnalyzer/src/main/java/SentimentAnalyzer.java
@@ -0,0 +1,71 @@
+/**
+ * Template from https://blog.openshift.com/day-20-stanford-corenlp-performing-sentiment-analysis-of-twitter-using-java/
+ * Modified by Santiago Estens
+ */
+
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+
+public class SentimentAnalyzer {
+
+    public int findSentiment(String line) {
+
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        int mainSentiment = 0;
+        if (line != null && line.length() > 0) {
+            int longest = 0;
+            Annotation annotation = pipeline.process(line);
+            for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
+                Tree tree = sentence.get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
+                int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
+                String partText = sentence.toString();
+                if (partText.length() > longest) {
+                    mainSentiment = sentiment;
+                    longest = partText.length();
+                }
+
+            }
+        }
+        // If the model returns a sentiment outside the predefined bounds, return -1 (error)
+        if (mainSentiment > 4 || mainSentiment < 0) {
+            return -1;
+        }
+
+        return mainSentiment;
+
+    }
+    // Interpret the numerical value the model returns
+    private String sentimentToString(int sentiment) {
+        switch (sentiment) {
+            case 0:
+                return "Very Negative";
+            case 1:
+                return "Negative";
+            case 2:
+                return "Neutral";
+            case 3:
+                return "Positive";
+            case 4:
+                return "Very Positive";
+            default:
+                return "";
+        }
+    }
+
+    public static void main(String[] args) {
+        SentimentAnalyzer sentimentAnalyzer = new SentimentAnalyzer();
+        int sentiment = sentimentAnalyzer.findSentiment("The weather is excellent today");
+        System.out.println(sentiment);
+        System.out.println(sentimentAnalyzer.sentimentToString(sentiment));
+
+    }
+}
